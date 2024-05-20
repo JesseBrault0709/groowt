a2c93f080ca417bc30878803593462dee2b9dfb3
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2024-05-20 14:54:24 +0200

    Removed unused classes.

--- web-view-components-compiler/src/main/java/groowt/view/component/web/transpile/SourceMapDeserializer.java
+++
@@ -1,8 +0,0 @@
-package groowt.view.component.web.transpile;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface SourceMapDeserializer {
-    List<SourceMap.SourceMapEntry> deserialize(String serializedSourceMap);
-}
