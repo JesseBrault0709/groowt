752d58c40a61bf7b44d101339aa2432203720a9f
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2024-05-21 08:13:09 +0200

    Fixed up node util.

--- web-view-components-compiler/src/main/java/groowt/view/component/web/ast/NodeUtil.kt
+++ web-view-components-compiler/src/main/java/groowt/view/component/web/ast/NodeUtil.kt
@@ -1,11 +1,45 @@
+@file:JvmName("NodeUtil")
 package groowt.view.component.web.ast
 
 import groowt.view.component.web.antlr.TokenList
 import groowt.view.component.web.antlr.formatToken
 import groowt.view.component.web.ast.node.LeafNode
 import groowt.view.component.web.ast.node.Node
+import groowt.view.component.web.ast.node.TreeNode
 
-fun formatSingleNode(node: Node, sb: StringBuilder, indentTimes: Int, indent: String, tokenList: TokenList) {
+fun getDeepestLeftmost(node: Node): LeafNode? {
+    return when (node) {
+        is LeafNode -> node
+        is TreeNode -> {
+            if (node.children.isNotEmpty()) {
+                getDeepestLeftmost(node.children.first())
+            } else {
+                null
+            }
+        }
+    }
+}
+
+fun formatAst(node: Node, tokenList: TokenList): String {
+    val sb = StringBuilder()
+    formatAst(node, tokenList, sb, indentTimes = 0, indent = "  ")
+    return sb.toString()
+}
+
+fun formatAst(
+    node: Node,
+    tokenList: TokenList,
+    sb: StringBuilder,
+    indentTimes: Int = 0,
+    indent: String = "  ",
+) {
+    formatSingleNode(node, sb, indentTimes, indent, tokenList)
+    if (node is TreeNode) {
+        node.children.forEach { formatAst(it, tokenList, sb, indentTimes + 1, indent) }
+    }
+}
+
+private fun formatSingleNode(node: Node, sb: StringBuilder, indentTimes: Int, indent: String, tokenList: TokenList) {
     sb.append(indent.repeat(indentTimes))
     sb.append(
         "${node.javaClass.simpleName}(${node.tokenRange.startPosition.toStringShort()}.."
